19aea384be2b1ea844c42048073107833b31ced1
Rachel Schieferstein and sommer howe <sommer@>
2020-01-13 11:19:32 -0800

    added c to f formulas to calculator js

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ var add = function(number1, number2) {
     return number1 + number2;
     };
     
-    var number1 = parseInt(prompt("Enter weight (in kg):"));
+    var number1 = parseInt(prompt("Enter temp"));
     var number2 =parseFloat(prompt("Enter height (in meters):"));
     
 
@@ -22,8 +22,13 @@ var addresult = add(number1, number2);
 var subresult = sub(number1, number2);
 var multiresult = multi(number1, number2);
 var divresult = div(number1, number2);
+
 var bmiresult= (number1 / (number2 * number2));
 
+var cnum2 = ((number1 + number1) * 0.1);
+var temp = ((number1 + number1) - cnum2 + 32);
 
 
-alert(bmiresult);
+
+
+alert(temp);
